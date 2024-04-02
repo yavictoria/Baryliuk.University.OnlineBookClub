c7a16446c48659beb333c7f886a741f981f92d0a
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-04-02 16:46:35 +0300

    add comments features

--- group/urls.py
+++ group/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('', views.group_list, name='list'),
     path('group_create', views.group_create, name='group_create'),
     path('show/<int:id>', views.show, name='show'),
+    path('show/add_user', views.add_user, name='add_user'),
+    path('show/comment_create', views.comment_create, name='comment_create'),
+    path('show/comment_delete/<int:id>', views.comment_delete, name='comment_delete'),
+    path('show/delete_user', views.delete_user, name='delete_user'),
     path('group_delete/<int:id>', views.group_delete, name='group_delete'),
     path('edit/<int:id>', views.edit, name='edit'),
 ]
