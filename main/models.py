f098c8cce36c01a44fc7b0379e27d3cb7d0fa8d3
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-05-21 19:36:23 +0300

    add report system feature

--- main/models.py
+++ main/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    feedback = models.TextField(max_length=1000, blank=False)
+    user_id = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.feedback)
+
+
+
+class Report(models.Model):
+    report = models.TextField(max_length=1000, blank=False)
+    user_id = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, blank=False, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.report)
+
--- manager/urls.py
+++ manager/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('users_list', views.users_list, name='users_list'),
     path('group_list', views.group_list, name='group_list'),
+    path('feedback_list', views.feedback_list, name='feedback_list'),
+    path('report_list', views.report_list, name='report_list'),
     path('group_show/<int:id>', views.group_show, name='group_show'),
+    path('report_list/report_delete/<int:id>', views.report_delete, name='report_delete'),
 
 ]
 
